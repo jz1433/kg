19637b29d96d7deac071929adddea4a189ebec10
JennyZ <64447128+jz1433@users.noreply.github.com>
2021-04-30 21:08:18 -0400

    add an array variable called nums to hold the integers entered by users from the command line

---
+++ main.js
@@ -0,0 +1,10 @@
+// given an array of integers
+var nums = process.argv.slice(2).map(function(num){
+  return parseInt(num, 10);
+});
+
+// write a function takes an array of integers and convert into an array of phonetic strings
+// function: numberConverter
+
+
+// print out the result by calling the function
