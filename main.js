f250d8c69370b55c44342a82b623a740f39d8585
JennyZ <64447128+jz1433@users.noreply.github.com>
2021-04-30 21:16:57 -0400

    add a mapping called numNameMap to map numbers to their phonetic names

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ var nums = process.argv.slice(2).map(function(num){
 
 // write a function takes an array of integers and convert into an array of phonetic strings
 // function: numberConverter
+function numberConverter(numList){
+  const numNameArray = [['0', 'Zero'], ['1', 'One'], ['2', 'Two'], ['3', 'Three'],
+  ['4', 'Four'], ['5', 'Five'], ['6', 'Six'], ['7', 'Seven'], ['8', 'Eight'],
+  ['9', 'Nine']];
+  const numNameMap = new Map(numNameArray);
+  var res = [];
 
+  return res;
+}
 
 // print out the result by calling the function
